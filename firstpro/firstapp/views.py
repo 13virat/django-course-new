2ab14dc058f640a8ac773e245deb0a326a427b5d
13virat <viratgupta1314@gmail.com>
2024-04-23 23:01:06 +0530

    14

--- firstpro/firstapp/views.py
+++ firstpro/firstapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+# from django.http import HttpResponse 
 
 # Create your views here.
 def index(request):
     # return HttpResponse("hello world")
-    return render(request, "index.html")
+    my_dict={'insert_me':"hello world"}
+    return render(request, "firstapp/index.html",context=my_dict)
+    

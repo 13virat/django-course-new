5b3a913ab85e90be88efaf22d93c0fa2935db403
13virat <viratgupta1314@gmail.com>
2024-04-26 19:21:03 +0530

    15

--- firstpro/firstapp/admin.py
+++ firstpro/firstapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from firstapp.models import Topic,Webpage,AcessRecord
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(AcessRecord)
 
 # Register your models here.

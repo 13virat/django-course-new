c107c278d6f3b0141dd2622da22eb71fa5bca41d
13virat <viratgupta1314@gmail.com>
2024-05-02 23:50:11 +0530

    34

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import View,TemplateView
 
 # Create your views here.
+class CBView(View):
+    def get(self,request):
+        return HttpResponse("CLASS BASED VIEWS ARE COOL!")

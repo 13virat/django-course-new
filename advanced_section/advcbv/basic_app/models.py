65560fbb0d156d443930dc19531a313b32e759eb
13virat <viratgupta1314@gmail.com>
2024-05-26 17:49:03 +0530

    back

--- advanced_section/advcbv/basic_app/models.py
+++ advanced_section/advcbv/basic_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name=models.CharField(max_length=264,unique=True)
+    principal=models.CharField(max_length=264,unique=True)
+    location=models.CharField(max_length=264,unique=True)
+    def __str__(self):
+        return self.name
+    
+
+class Student(models.Model):
+    name=models.CharField(max_length=264,unique=True)
+    age=models.PositiveIntegerField()
+    school=models.ForeignKey(School,on_delete=models.CASCADE,related_name='students')
+
+    def __str__(self):
+        return self.name

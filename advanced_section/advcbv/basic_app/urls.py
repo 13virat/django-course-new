fd177a647a62b10d9ee6e0faf93147139bc3ba2a
13virat <viratgupta1314@gmail.com>
2024-05-27 11:02:53 +0530

    crud

--- advanced_section/advcbv/basic_app/urls.py
+++ advanced_section/advcbv/basic_app/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     
     path("",views.SchoolListView.as_view(),name='list'),
     path("<int:pk>/",views.SchoolDetailView.as_view(),name='detail'),
-    # path("<int:pk>/update/",views.SchoolUpdateView.as_view(),name='update'),
-    # path("<int:pk>/delete/",views.SchoolDeleteView.as_view(),name='delete'),
+    path("update/<int:pk>/",views.SchoolUpdateView.as_view(),name='update'),
+    path("delete/<int:pk>/",views.SchoolDeleteView.as_view(),name='delete'),
     # path("<int:school_id>/student_list/",views.StudentListView.as_view(),name='student_list'),/",views.SchoolDetailView.as_view(),name='detail'),
-    
+    path("create/",views.SchoolCreateView.as_view(),name='create'),
 ]
